a19a0ef76c27bfb3a94a45165439106b7dffbdd7
Administrator <zuhao.peng@aliyun.com>
2018-03-06 17:24:09 +0800

    1

--- projectone/appone/models.py
+++ projectone/appone/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
--- projectone/appone/views.py
+++ projectone/appone/views.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from appone import models
 from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+
+# def ind(request):
+#     return render(request, "ind.html")
 
 
-def ind(request):
-    return render(request, "index.html")
+def index(request):
+    if request.method == 'POST':
+        username =  request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        models.UserInfo.objects.create(user=username, pwd=password)
+    userlist = models.UserInfo.objects.all()
+    return render(request, "index.html", {"data": userlist})

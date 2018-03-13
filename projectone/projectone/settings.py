b6aad990885e2ace9420a2690d8ddf19d73a659b
Administrator <zuhao.peng@aliyun.com>
2018-03-13 11:24:19 +0800

    3.13

--- projectone/projectone/settings.py
+++ projectone/projectone/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # LANGUAGE_CODE = 'zh-hans'
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
--- projectone/sockalert/admin.py
+++ projectone/sockalert/admin.py
@@ -16,16 +16,23 @@ class disp(admin.ModelAdmin):
     ordering = ('id',)   # -id
     # list_editable 设置默认可编辑字段
     list_editable = ['data']
+    # 设置哪些字段可以点击进入编辑界面
+    list_display_links = ('url',)
+    # 筛选器
+    list_filter = ('url',)  # 过滤器
+    search_fields = ('url',)  # 搜索字段
+    date_hierarchy = 'crttime'  # 详细时间分层筛选　
 
 
 @admin.register(yx_sp)
 class disp(admin.ModelAdmin):
     list_display = ('id', 'url', 'data', 'crttime')
-    # list_per_page设置每页显示多少条记录，默认是100条
     list_per_page = 20
-    # ordering设置默认排序字段，负号表示降序排序
-    ordering = ('id',)  # -id
-    # list_editable 设置默认可编辑字段
+    ordering = ('id',)
     list_editable = ['data']
+    list_display_links = ('url',)
+    list_filter = ('url',)  # 过滤器
+    search_fields = ('url',)  # 搜索字段
+    date_hierarchy = 'crttime'  # 详细时间分层筛选　
 
 
